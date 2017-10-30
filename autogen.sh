ad296fad893a66a398a35126b542d5941bae4c28
Emmanuele Bassi <ebassi@gnome.org>
2017-10-30 17:30:28 +0000

    build: Clean up autogen
    Allow non-srcdir builds, check for dependencies, and follow the GNOME
    standard of NOCONFIGURE.

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,26 @@
 #!/bin/sh
-libtoolize && intltoolize && aclocal && automake --add-missing && autoconf
+
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
+olddir=`pwd`
+cd "$srcdir"
+
+INTLTOOLIZE=`which intltoolize`
+if test -z $INTLTOOLIZE; then
+        echo "*** Unable to find intltoolize; you must install intltool ***"
+        exit 1
+fi
+
+AUTORECONF=`which autoreconf`
+if test -z $AUTORECONF; then
+        echo "*** Unable to find autoreconf; you must install Autotools ***"
+        exit 1
+fi
+
+${INTLTOOLIZE} || exit $?
+${AUTORECONF} -f -v -i || exit $?
+
+cd "$olddir"
+
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
