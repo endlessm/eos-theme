2763837b3ecab604085f8375eba8490cfed42f10
Andre Moreira Magalhaes <andre.magalhaes@endlessos.org>
2021-02-04 17:38:57 -0300

    Drop i18n support
    This is currently failing with automake 1.16.1 when srcdir != builddir:
      srcdir=../../po /usr/bin/intltool-update -m
      The following files contain translations and are currently not in use. Please
      consider adding these to the POTFILES.in file, located in the po/ directory.
    
      build_/icons/index.theme.in
    
    Given we don't carry any translation atm, let's remove this support
    for now to avoid unnecessary maintenance and it can be reinstated
    later if needed.
    
    https://phabricator.endlessm.com/T31300

--- autogen.sh
+++ autogen.sh
@@ -6,19 +6,12 @@ test -n "$srcdir" || srcdir=.
 olddir=`pwd`
 cd "$srcdir"
 
-INTLTOOLIZE=`which intltoolize`
-if test -z $INTLTOOLIZE; then
-        echo "*** Unable to find intltoolize; you must install intltool ***"
-        exit 1
-fi
-
 AUTORECONF=`which autoreconf`
 if test -z $AUTORECONF; then
         echo "*** Unable to find autoreconf; you must install Autotools ***"
         exit 1
 fi
 
-${INTLTOOLIZE} || exit $?
 ${AUTORECONF} -f -v -i || exit $?
 
 cd "$olddir"
